14e296b359b2609d341803bc37eb2b2d51dee098
conrad <whalliburton@gmail.com>
2016-02-29 12:15:36 -0700

    press-release as html

--- press-release.lisp
+++ press-release.lisp
@@ -5,11 +5,9 @@
     (header-panel
       (toolbar
         (icon-button :class "toolbar-icon" :style "margin-left:0px;" :icon "arrow-back" :onclick (ps (select-page 0))))
-      (:pre :style "padding:20px;"
-
-       "FOR IMMEDIATE RELEASE (not really!, still a draft)
-
-Event Name: Missoula Civic Hackathon
+      (:div :style "padding:20px;"
+            (:h1 "FOR IMMEDIATE RELEASE")
+            (:pre "Event Name: Missoula Civic Hackathon
 
 Event Dates: March 26-27, 2016
 
@@ -24,106 +22,108 @@ Phone Number: (406) 830-5031
 
 E-mail Address: <a href=\"mailto:will@blueskystewardship.org\">will@blueskystewardship.org</a>
 
-Website Address: <a href=\"http://missoulacivichackathon.org\">http://missoulacivichackathon.org</a>
+Website Address: <a href=\"http://missoulacivichackathon.org\">http://missoulacivichackathon.org</a>")
 
-Missoula Civic Hackathon Hosts Area Technical Developers and Citizens
-to Build Solutions to Civic Problems
+            (:br)
+            (:h1 "Missoula Civic Hackathon Hosts Area Technical Developers and Citizens
+to Build Solutions to Civic Problems")
 
-MISSOULA, MT
+            (:h1 "MISSOULA, MT")
 
-What's happening and who's it for?
+            (:h2 "What's happening and who's it for?")
 
-Missoula is set to host its first Civic Hackathon from March 26 through
-the 27th, 2016. Civic Hackathons are events where developers,
-designers, artists, students, researchers, civic leaders, and citizens
-come together to share ideas, form teams, and work towards tangible
-solutions to the problems we all face daily in the towns we
-live. Local technical experts, professors and civic leaders will be on
-hand to coach teams.
+            (:p "Missoula is set to host its first Civic Hackathon from
+March 26 through the 27th, 2016. Civic Hackathons are
+events where developers, designers, artists, students,
+researchers, civic leaders, and citizens come together to
+share ideas, form teams, and work towards tangible
+solutions to the problems we all face daily in the towns
+we live. Local technical experts, professors and civic
+leaders will be on hand to coach teams.")
 
-\"The opportunity to meet other developers and people with pressing
-civic problems and to work towards a common goal, while at the same
-time having fun, learning much, and making friends, is truly valuable
-and often-times rarely experienced.\" says William Halliburton who
-attended Spokane SpoCode Civic Hackathon 2014.
+            (:p "\"The opportunity to meet other developers and people with
+pressing civic problems and to work towards a common goal, while at
+the same time having fun, learning much, and making friends, is truly
+valuable and often-times rarely experienced.\" says William
+Halliburton who attended Spokane SpoCode Civic Hackathon 2014.")
 
-\"Although I am not a computer programmer, I love the idea of working
+            (:p "\"Although I am not a computer programmer, I love the idea of working
 with fellow Missoulians to accomplish something for this great city
 that will serve not just this locality but potentially localities all
 over. If we can solve a problem here, who's to say that it can't at
 the very least be tested in other cities.\" says community member Luke
-Robinson.
+Robinson.")
 
-<< TODO add one more quotes >>
+            "<< add one more quote >>"
 
-Why attend a Civic Hackathon Event?
+            (:h2 "Why attend a Civic Hackathon Event?")
 
-   + Education - learn by doing.
-   + Build your network - meet talented developers and civic changers.
-   + Team dating - build your team.
-   + Learn a new skill - flex your technical and social muscles.
-   + Join a global community - over hundreds of civic hackathons
-     past, present, and future
+            (:ul
+             (:li "Education - learn by doing.")
+             (:li "Build your network - meet talented developers and civic changers.")
+             (:li "Team dating - build your team.")
+             (:li "Learn a new skill - flex your technical and social muscles.")
+             (:li "Join a global community - over hundreds of civic hackathons past, present, and future."))
 
-How is it done?
+            (:h2 "How is it done?")
 
-Beginning with an <a style=\"display:inline-block\" href=\"https://en.wikipedia.org/wiki/Open_Space_Technology\">open space technology</a>
+            (:p "Beginning with an <a style=\"display:inline-block\" href=\"https://en.wikipedia.org/wiki/Open_Space_Technology\">open space technology</a>
 unconference on Saturday morning, attendees bring their best ideas and
 inspire others to join their team. Over Saturday and Sunday teams
 focus on developing solutions, validating their ideas, and building a
 working prototype. On Sunday evening teams demo their prototypes and
-receive valuable feedback from a panel of experts.
+receive valuable feedback from a panel of experts.")
 
-When and where is it?
+            (:h2 "When and where is it?")
 
-The detailed Missoula Civic Hackathon Schedule can be found online at
+            (:p "The detailed Missoula Civic Hackathon Schedule can be found online at
 <a href=\"http://missoulacivichackathon/\">http://missoulacivichackathon/</a>. The hours of the event are Saturday,
 March 26, 2016 from 9:00 a.m. through the night to 6:00 p.m Sunday,
 March 27.  It will be held on the University of Montana Campus at the
 Phillis J. Washington Education Center, 32 Campus Drive, Missoula,
-Montana 59812.
+Montana 59812.")
 
-What Are Other Civic Hackathons Accomplishing?
+            (:h2 "What Are Other Civic Hackathons Accomplishing?")
 
-<< add in some descriptions of current and past hackathon projects >>
+            "<< add in some descriptions of current and past hackathon projects >>"
 
-How can I sign up?
+            (:h2 "How can I sign up?")
 
-Interested parties can register at <a href=\"http://missoulacivichackathon/\">http://missoulacivichackathon/</a>
+            (:p "Interested parties can register at <a href=\"http://missoulacivichackathon/\">http://missoulacivichackathon/</a>
 Registration includes breakfast, lunch, and dinner on Saturday as well
 as breakfast and lunch on Sunday. Attendees will also receive
-exclusive Missoula Civic Hackathon perks.
+exclusive Missoula Civic Hackathon perks.")
 
-How are we attempting to mitigate our ecological footprint?
+            (:h2 "How are we attempting to mitigate our ecological footprint?")
 
-The Missoula Civic Hackathon is a Zero-Waste sponsored event and
+            (:p "The Missoula Civic Hackathon is a Zero-Waste sponsored event and
 dinnerware and composting services will be provided. A ride and couch
 sharing forum is located online at <<insert link>> that participants
 traveling from other cities are encouraged to utilize. All meat served
-at the event is locally grown, grass-fed and grass-finished.
+at the event is locally grown, grass-fed and grass-finished.")
 
+            (:h2 "About our local sponsors:")
 
-About our local sponsors:
-
-Thanks to our local sponsors the Washington Companies, Phillis
+            (:p "Thanks to our local sponsors the Washington Companies, Phillis
 J. Washington Foundation, Moonlight Kitchens, Nora McDougall-Collins
 Cartering , Blue Sky Stewardship, Montana Code School, and the
-University of Montana.
+University of Montana.")
 
-Sponsorship opportunities still available, please contact
-us at <a href=\"mailto:info@blueskystewardship.org\">info@blueskystewardship.org</a>.
+            (:p "Sponsorship opportunities still available, please contact
+us at <a href=\"mailto:info@blueskystewardship.org\">info@blueskystewardship.org</a>.")
 
-ABOUT BLUE SKY STEWARDSHIP: Blue Sky Stewardship
-<a href=\"http://blueskystewardship.org\">http://blueskystewardship.org</a> is a local food non-profit focused on
+            (:h2 "ABOUT BLUE SKY STEWARDSHIP:")
+
+            (:p "<a href=\"http://blueskystewardship.org\">Blue Sky Stewardship</a> is a local food non-profit focused on
 researching and demonstrating solutions to Missoula's food production,
 processing, distribution, and waste systems. Blue Sky Stewardship is
 supported by the High Stakes Foundation, the Montana Institute for
 Sustainable Transportation, generous private donors, and a team of
-volunteers.
-
-")
+volunteers.")))))
 
 
-          )
-    )
-  )
+
+
+
+
+
