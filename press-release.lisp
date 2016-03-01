8f2a471223600b2b550bf80a0d5191d85bad2ae4
conrad <whalliburton@gmail.com>
2016-02-29 18:14:48 -0700

    fix sponsors

--- press-release.lisp
+++ press-release.lisp
@@ -105,10 +105,10 @@ at the event is locally grown, grass-fed and grass-finished.")
 
             (:h2 "About our local sponsors:")
 
-            (:p "Thanks to our local sponsors the Washington Companies, Phillis
-J. Washington Foundation, Moonlight Kitchens, Nora McDougall-Collins
-Catering , Blue Sky Stewardship, Montana Code School, and the
-University of Montana.")
+            (:p "Thanks to our local sponsors the Washington
+Companies, Dennis & Phyllis Washington Foundation, Moonlight Kitchens,
+Nora McDougall-Collins Catering , Blue Sky Stewardship, Montana Code
+School, and the University of Montana.")
 
             (:p "Sponsorship opportunities are still available,
 interested parties please contact us at <a href=\"mailto:info@blueskystewardship.org\">info@blueskystewardship.org</a>.")
