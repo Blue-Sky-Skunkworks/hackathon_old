0f8bffc50262647f2090773f40640842c96d0deb
conrad <whalliburton@gmail.com>
2016-02-29 21:35:57 -0700

    minor

--- sponsors.lisp
+++ sponsors.lisp
@@ -2,7 +2,7 @@
 
 (defun render-sponsors (stream)
   (html
-    (header-panel
+    (header-panel :mode "seamed"
       (toolbar :class "sponsors"
         (:span :style "margin-left:0px;" :class "title" "Missoula Civic Hackathon Sponsors")
         (icon-button :class "toolbar-icon" :style "margin-left:0px;" :icon "arrow-back" :onclick (ps (select-page 0))))
