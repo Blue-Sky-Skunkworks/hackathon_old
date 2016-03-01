b353ee12b3e08ab798ae3196fa7e8b6d00b3904c
conrad <whalliburton@gmail.com>
2016-03-01 10:51:37 -0700

    properly serve "/"

--- server.lisp
+++ server.lisp
@@ -5,10 +5,20 @@
 (defclass web-acceptor (hunchentoot:acceptor)
   ((dispatch-table :reader dispatch-table :initarg :dispatch-table)))
 
+(defun create-exact-dispatcher (name handler)
+  "Creates a request dispatch function which will dispatch to the
+function denoted by HANDLER if the file name of the current request
+matches NAME."
+  (lambda (request)
+    (and (equal name (script-name request))
+         handler)))
+
 (defun format-dispatch (dispatch)
   (if (consp dispatch)
       (ecase (first dispatch)
         (:prefix (hunchentoot:create-prefix-dispatcher (second dispatch) (third dispatch)))
+        (:exact (create-exact-dispatcher (second dispatch) (third dispatch)))
+        (:regex (hunchentoot:create-regex-dispatcher (second dispatch) (third dispatch)))
         (:folder (hunchentoot:create-folder-dispatcher-and-handler (second dispatch) (third dispatch)))
         (:static (hunchentoot:create-static-file-dispatcher-and-handler (second dispatch) (third dispatch))))
       dispatch))
@@ -23,13 +33,15 @@
                        :access-log-destination (hackathon-file (format nil "log/access-~A.log" (now)))
                        :message-log-destination (hackathon-file (format nil "log/message-~A.log" (now)))
                        :dispatch-table (mapcar 'format-dispatch
-                                               `((:folder "/" ,(hackathon-file "build/"))))))
+                                               `((:exact "/" render-front-page)
+                                                 (:folder "/" ,(hackathon-file "build/"))))))
   (hunchentoot:start *web-acceptor*))
 
 (defmethod hunchentoot:acceptor-dispatch-request ((acceptor web-acceptor) request)
-  (bugout acceptor (dispatch-table acceptor) request)
+  (bugout (request-uri*))
   (iter (for dispatcher in (dispatch-table acceptor))
         (when-let (action (funcall dispatcher request))
+          (bugout action)
           (return (funcall action)))
         (finally (call-next-method))))
 
