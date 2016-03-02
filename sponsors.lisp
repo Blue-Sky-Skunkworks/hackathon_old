3f255325417f59a18f2e227bf4d0c2bf8345c71c
conrad <whalliburton@gmail.com>
2016-03-01 19:14:18 -0700

    update sponsors

--- sponsors.lisp
+++ sponsors.lisp
@@ -6,18 +6,21 @@
       (toolbar :class "sponsors"
         (:span :style "margin-left:0px;" :class "title" "Missoula Civic Hackathon Sponsors")
         (icon-button :class "toolbar-icon" :style "margin-left:0px;" :icon "arrow-back" :onclick "page(\"/\");"))
-      (:div :style "padding:20px;" :id "sponsors"
-            (iter (for (name url image) in
-                       '(("Washington Companies" "http://www.washcorp.com/" "WC")
-                         ("Dennis & Phyllis Washington Foundation" "http://www.dpwfoundation.org/" ("DPW"))
-                         ("Moonlight Kitchens" "http://moonlightkitchens.com/" "MK")
-                         ("Nora McDougall-Collins Catering" "http://montanawebmaster.com/")
-                         ("Blue Sky Stewardship" "http://blueskystewardship.org" "BSS")
-                         ("Montana Code School" "http://montanacodeschool.com/" "MCS")
-                         ("The University of Montana" "http://www.umt.edu/" ("UM"))))
-                  (card :class "card"
-                        (:div :class (concatenate 'string "card-content" (when (consp image) " grey"))
-                              (:a :target "_blank" :href url
-                                  (if image
-                                      (htm (:img :alt name :src (format nil "images/sponsors/~A.png" (if (consp image) (car image) image))))
-                                      (esc name))))))))))
+      (:div :style "padding:20px;" :class "layout vertical"
+            (:h1 "Thanks to all of the Missoula Civic Hackathon's sponsors")
+            (vertical-break "30px")
+            (:div :id "sponsors"
+             (iter (for (name url image) in
+                        '(("Washington Companies" "http://www.washcorp.com/" "WC")
+                          ("Dennis & Phyllis Washington Foundation" "http://www.dpwfoundation.org/" ("DPW"))
+                          ("Moonlight Kitchens" "http://moonlightkitchens.com/" "MK")
+                          ("Nora McDougall-Collins Catering" "http://montanawebmaster.com/")
+                          ("Blue Sky Stewardship" "http://blueskystewardship.org" "BSS")
+                          ("Montana Code School" "http://montanacodeschool.com/" "MCS")
+                          ("The University of Montana" "http://www.umt.edu/" ("UM"))))
+                   (card :class "card"
+                         (:div :class (concatenate 'string "card-content" (when (consp image) " grey"))
+                               (:a :target "_blank" :href url
+                                   (if image
+                                       (htm (:img :alt name :src (format nil "images/sponsors/~A.png" (if (consp image) (car image) image))))
+                                       (esc name)))))))))))
