9cffafa45392c7c9c79c13af3ac60d46a8d72734
conrad <whalliburton@gmail.com>
2016-03-02 17:17:26 -0700

    update schedule

--- schedule.lisp
+++ schedule.lisp
@@ -25,5 +25,5 @@
                               (:h3 "<span class='schedule'>8:00 AM</span>Breakfast")
                               (:h3 "<span class='schedule'>Noon</span>Lunch")
                               (:h3 "<span class='schedule'>2:00 PM</span>Last commit")
-                              (:h3 "<span class='schedule'>2:00 PM - 5:30 PM</span>Team Presentations")
+                              (:h3 "<span class='schedule'>2:30 PM - 5:30 PM</span>Team Presentations")
                               (:h3 "<span class='schedule'>5:30 PM</span>Dinner and Award Presentation"))))))
