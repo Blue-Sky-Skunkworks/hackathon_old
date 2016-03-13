1114c23f36fc819955f79898bc52e3600412db09
conrad <whalliburton@gmail.com>
2016-03-13 14:06:35 -0600

    centering in a 700px column

--- sharing.lisp
+++ sharing.lisp
@@ -6,9 +6,10 @@
       (toolbar :class "sharing"
         (:span :style "margin-left:0px;" :class "title" "Missoula Civic Hackathon Ride & Couch Sharing")
         (icon-button :class "toolbar-icon" :style "margin-left:0px;" :icon "arrow-back" :onclick "page(\"/\");"))
-      (:div :style "padding:20px;"
-            "Please use the <a target='_blank'
+      (:div :style "padding:20px;" :class "layout vertical center"
+            (:div :style "padding-top:60px;max-width:700px;"
+                  "Please use the <a target='_blank'
             href='https://groups.google.com/forum/#!forum/blue-sky-skunkworks'>mailing
             list</a> for ride and couch requests. <br><br> We have at
             least 4 spaces free at the moment for sleeping during the
-            Hackathon."))))
+            Hackathon.")))))
