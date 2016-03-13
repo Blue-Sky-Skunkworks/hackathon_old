f9db9c5e0935cf988d9e8057faa106afa8a95500
conrad <whalliburton@gmail.com>
2016-03-13 14:38:46 -0600

    more participate

--- participate.lisp
+++ participate.lisp
@@ -11,12 +11,11 @@
                               "We have set up a <a
                         href='https://plus.google.com/events/c8g3dfagp5l86nfbol4mf44e7sc'
                         target='_blank'>Google Hangouts</a> that will
-                        be live and streaming from one of the large
+                        be streaming live from one of the large
                         smartboards in the main hall. We can set up
                         others in each space as needed, but this will
                         be the main jumping off point for people
                         wishing to communicate to the live hackathon
                         and for us to communicate to you.")
-                        (vertical-break "40px")
-                        (:iframe :width "560" :height "315" :src "//www.youtube.com/embed/_-zAVk3GVAs" :frameborder "0" :allowfullscreen t)
-                        ))))
+                        (vertical-break "60px")
+                        (:iframe :width "560" :height "315" :src "//www.youtube.com/embed/_-zAVk3GVAs" :frameborder "0" :allowfullscreen t)))))
