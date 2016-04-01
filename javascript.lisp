858227ea8bcce295e798a2e6b80d23a267cd60e6
conrad <whalliburton@gmail.com>
2016-04-01 16:09:55 -0600

    life

--- javascript.lisp
+++ javascript.lisp
@@ -236,15 +236,35 @@
                   (@ img src) "/images/logo-cell.png")
             ((@ img add-event-listener) "load" animate-logo-go)))
 
+        (defvar *life-size* 8)
+
+        (defvar *life* (new (*uint8-array (* *life-size* *life-size*))))
+
+        (defun life (row col) (return (aref *life* (+ (* row *life-size*) col))))
+
+        (defun setup-life ()
+          (loop for row from 0 to (- *life-size* 1)
+             do (loop for col from 0 to (- *life-size* 1)
+                   do (setf (aref *life* (+ (* row *life-size*) col))
+                            (if (or (= col 0) (= row 0) (= row (- *life-size* 1)) (= col (- *life-size* 1))
+                                    (and (= col 1) (= row 1))
+                                    (and (= col 1) (= row (- *life-size* 2)))
+                                    (and (= col (- *life-size* 2)) (= row (- *life-size* 2)))
+                                    (and (= col (- *life-size* 2)) (= row 1)))
+                                (if (< ((@ *math random)) 0.88) 1 0)
+                                1)))))
+
         (defun animate-logo-go ()
           (let* ((canvas (get-by-id "logo"))
                  (cx ((@ canvas get-context) "2d")))
+            (setup-life)
+            ((@ cx clear-rect) 0 0 (@ canvas width) (@ canvas height))
             (loop
                for row from 0 to 7
                do
                  (loop for col from 0 to 7
-                    do ((@ cx draw-image) *logo-cell* (+ 1 (* col 43)) (+ 2 (* row 42)))))
-
+                    do (when (life row col)
+                         ((@ cx draw-image) *logo-cell* (+ 1 (* col 43)) (+ 2 (* row 42))))))
 
             (setf (@ cx fill-style) "white"
                   (@ cx stroke-style) "white")
@@ -258,6 +278,7 @@
             ((@ cx line-to) 173 181)
             ((@ cx fill))
 
+            ((@ cx save))
             ((@ cx begin-path))
             ((@ cx move-to) 173 164)
             ((@ cx line-to) 0 164)
@@ -269,5 +290,9 @@
             ((@ cx line-to) 173 198)
             ((@ cx clip))
 
-            (arc cx 173 181 62 :line-width 29))))))))
+            (arc cx 173 181 62 :line-width 29)
+            ((@ cx restore)))
+
+          (set-timeout (lambda () (animate-logo-go)) 6000)
+          ))))))
 
