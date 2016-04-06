ae76fd48a4f4a21bb9082c9f20cff74d7df452b1
conrad <whalliburton@gmail.com>
2016-04-06 12:20:37 -0600

    time update

--- time.lisp
+++ time.lisp
@@ -8,7 +8,7 @@
                            (icon-button :class "toolbar-icon" :style "margin-left:0px;" :icon "arrow-back" :onclick "page(\"/\");"))
                   (:div :style "padding:20px;background:black;" :class "fit layout vertical center-justified"
                         (:center
-                         (:div :style "width:590px;height:590px;overflow:hidden;"
-                               (:img :style "margin:-215px 0 0 -215px;" :width 1024 :height 1024
+                         (:div :style "width:630px;height:630px;overflow:hidden;"
+                               (:img :style "margin:-200px 0 0 -200px;" :width 1024 :height 1024
                                      :src "http://api.usno.navy.mil/imagery/moon.png")))))))
 
