78e091cb851958f0b6d1a08d32112e87f3d6fc2b
conrad <whalliburton@gmail.com>
2016-04-11 10:48:19 -0600

    show unliked pages in toc

--- wiki.lisp
+++ wiki.lisp
@@ -18,6 +18,12 @@
       (push (subseq text (aref rstart 0) (aref rend 0)) rtn))
     (nreverse rtn)))
 
+(defun list-wiki-pages ()
+  (sort (mapcar #L((substitute #\space #\- (pathname-name %)))
+                (remove-if-not #L(equal (pathname-type %) "md")
+                               (cl-fad:list-directory *wiki-directory*)))
+        #'string<))
+
 (defun build-wiki-tree (&optional (root "Home"))
   (let ((hash (make-hash-table :test 'equal)))
     (labels ((recur (el &optional ignore)
@@ -30,7 +36,9 @@
                                  (member link ignore :test #'string=)
                                  (gethash link hash))
                           (collect (recur link links))))))))
-      (recur root))))
+      (nconc (recur root)
+             (list (list :hr))
+             (mapcar #'list (sort (set-difference (list-wiki-pages) (iter (for (k v) in-hashtable hash) (collect k)) :test 'equal) #'string<))))))
 
 (defun render-tree (stream fn tree)
   (labels ((recur (el level)
@@ -38,12 +46,6 @@
              (iter (for child in (cdr el))
                    (recur child (1+ level)))))
     (recur tree 0)))
-
-(defun list-wiki-pages ()
-  (sort (mapcar #L((substitute #\space #\- (pathname-name %)))
-                (remove-if-not #L(equal (pathname-type %) "md")
-                               (cl-fad:list-directory *wiki-directory*)))
-        #'string<))
 
 (defun render-wiki (stream)
   (html
@@ -79,13 +81,15 @@
 
 
 (defun render-wiki-tree-row (stream level name)
-  (let ((missing (equal (char name 0) #\*)))
-    (when missing (setf name (subseq name 2)))
-    (item
-      :class (when missing "wiki-missing")
-      :onclick (format nil "selectIlink(\"~A\");" name)
-      (:span :style (format nil "display:inline-block;width:~Apx;" (* (max 0 (1- level)) 20)))
-      (esc name))))
+  (if (eq name :hr)
+      (html (:hr))
+      (let ((missing (equal (char name 0) #\*)))
+        (when missing (setf name (subseq name 2)))
+        (item
+          :class (when missing "wiki-missing")
+          :onclick (format nil "selectIlink(\"~A\");" name)
+          (:span :style (format nil "display:inline-block;width:~Apx;" (* (max 0 (1- level)) 20)))
+          (esc name)))))
 
 ; (render-tree t (lambda (stream level name) (format stream "~A: ~A~%" level name)) (build-wiki-tree))
 
