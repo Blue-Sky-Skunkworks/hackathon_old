5136d15b4953edd122bf919b378ae534cb7f2afe
conrad <whalliburton@gmail.com>
2016-04-24 17:07:58 -0600

    move elisp to .el

--- hackathon.el
+++ hackathon.el
@@ -21,3 +21,17 @@
         ("MEETING" :foreground "forest green" :weight bold)
         ("PHONE" :foreground "forest green" :weight bold)))
 
+(defun setup-hackathon ()
+      (interactive)
+      (set-register ?a "http://missoulacivichackathon.org")
+      (set-register ?s "http://missoulacivichackathon.org/#!/sponsors")
+      (set-register ?p "http://missoulacivichackathon.org/#!/press-release")
+      (set-register ?t "https://www.eventbrite.com/e/missoula-civic-hackathon-2016-tickets-21898542129")
+      (set-register ?c "https://github.com/Blue-Sky-Skunkworks/hackathon")
+      (set-register ?w "https://github.com/Blue-Sky-Skunkworks/missoula-civic-hackathon-notes/wiki/Projects")
+      (set-register ?m "—")
+      (set-register ?n "–")
+      (set-register ?' "“”")
+
+      (yas-load-directory "~/p/hackathon/missoula-civic-hackathon-notes.wiki/snippets")
+      )
